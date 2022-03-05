9d64e31e3d7d2672090e078a80ef7dca92070c26
Mao Li <anrenyil@gmail.com>
2022-03-04 16:12:17 -0800

    rails CRUD

---
+++ rails_project/amazon-43/app/controllers/products_controller.rb
@@ -0,0 +1,56 @@
+class ProductsController < ApplicationController
+    before_action :find_product, only: [:edit, :update, :show, :destroy]
+
+    def index
+        @products = Product.all
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new product_params
+        if @product.save
+            # alert, notice 
+            # flash["notice"] = "Created!"
+            # flash.notice = "Created!"
+            redirect_to product_path(@product.id), notice: "Created"
+        else
+            render :new
+        end
+    end
+
+    def show
+        
+    end
+
+    def destroy
+        @product.destroy
+        redirect_to products_path, alert: "Deleted!"
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @product.update product_params
+            redirect_to product_path(@product.id)
+        else
+            render :edit
+        end
+    end
+
+
+
+    private
+    def product_params
+        params.require(:product).permit(:title,:description,:price)
+    end
+
+    def find_product
+        @product = Product.find(params[:id])
+    end
+    
+end
--- rails_project/amazon-43/config/routes.rb
+++ rails_project/amazon-43/config/routes.rb
@@ -6,4 +6,14 @@ Rails.application.routes.draw do
   post("/contact_submit",{to:"welcome#thankyou",as: :contact_submit})
   get("/new_bill",{to:"bill#new_bill"})
   get("/bill", {to: 'bill#result', as: :bill})
+
+  get("/products",to:"products#index")
+  get("/products/new",to:"products#new",as: :new_product)
+  post("/products",to:"products#create")
+
+  get("/products/:id",to:"products#show",as: :product)
+  delete("/products/:id",to:"products#destroy")
+
+  get("/products/:id/edit",to:"products#edit",as: :edit_product)
+  patch("/products/:id",to:"products#update")
 end
