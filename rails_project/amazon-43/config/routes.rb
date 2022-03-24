153bff3055df66e4982e16e2a0a3561767780808
Mao Li <anrenyil@gmail.com>
2022-03-24 14:42:17 -0700

    rails api lab 3

--- rails_project/amazon-43/config/routes.rb
+++ rails_project/amazon-43/config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :products
+      resource :session, only: [:create, :destroy]
     end
   end
 end
