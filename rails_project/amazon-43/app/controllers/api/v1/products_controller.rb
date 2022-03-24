a0fa266d901adfed4694a2140edcc3864d0755fb
Mao Li <anrenyil@gmail.com>
2022-03-24 14:40:47 -0700

    rails api lab 2

--- rails_project/amazon-43/app/controllers/api/v1/products_controller.rb
+++ rails_project/amazon-43/app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::ProductsController < ApplicationController
+    before_action :find_product, only: [:show]
     def index
         products = Product.order(created_at: :desc)
         render(json: products)
+    end
+
+    def show
+        render json: @product
+    end
+
+    private
+
+    def find_product
+      @product ||= Product.find params[:id]
     end
 end
