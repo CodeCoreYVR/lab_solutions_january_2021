525dfe79103326d1b03c5c767a44e2f13e5ce492
Mao Li <anrenyil@gmail.com>
2022-03-24 14:46:37 -0700

    rails api lab 4

--- rails_project/amazon-43/app/controllers/api/v1/products_controller.rb
+++ rails_project/amazon-43/app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :find_product, only: [:show]
     def index
         products = Product.order(created_at: :desc)
@@ -9,9 +10,26 @@ class Api::V1::ProductsController < ApplicationController
         render json: @product
     end
 
+    def create
+        product = Product.new product_params
+        product.user = current_user
+        if product.save
+          render json: { id: product.id }
+        else
+          render(
+            json: { errors: product.errors },
+            status: 422 
+          )
+        end
+    end
+
     private
 
     def find_product
       @product ||= Product.find params[:id]
+    end
+    
+    def product_params
+        params.require(:product).permit(:title, :description)
     end
 end
